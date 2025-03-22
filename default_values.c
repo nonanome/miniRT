06ad62f85122e193cac84b36dab4519edfaf2870
qhahn <qhahn@student.42.fr>
2025-03-22 19:32:17 +0100

    another great many norm fixes

--- default_values.c
+++ default_values.c
@@ -1,48 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   default_values.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qhahn <qhahn@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/22 19:26:19 by qhahn             #+#    #+#             */
+/*   Updated: 2025/03/22 19:26:30 by qhahn            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "miniRT.h"
 
-t_material default_material(void)
+t_material	default_material(void)
 {
-	t_material dm;
-	t_xyzvektor color;
+	t_material	dm;
+	t_xyzvektor	color;
 
 	color.x = 1;
 	color.y = 0;
 	color.z = 0;
 	color.w = 1;
-
 	dm.color = get_color_from_tuple(color);
 	dm.ambient = 0.1;
 	dm.diffuse = 0.9;
 	dm.specular = 0.9;
 	dm.shininess = 200.0;
 	dm.checker.enable = false;
-	return dm;
+	return (dm);
 }
 
-t_light default_light(void)
+t_light	default_light(void)
 {
-	t_light source;
-	t_xyzvektor color;
-	t_xyzvektor position;
+	t_light		source;
+	t_xyzvektor	color;
+	t_xyzvektor	position;
 
 	color.x = 1;
 	color.y = 1;
 	color.z = 1;
 	color.w = 1;
-
 	position.x = -10;
 	position.y = 10;
 	position.z = -10;
 	position.w = 0;
-
 	source.color = color;
 	source.position = position;
-	return source;
+	return (source);
 }
 
-t_sphere new_sphere()
+t_sphere	new_sphere(void)
 {
-	t_sphere result;
+	t_sphere	result;
 
 	result.origin.x = 0;
 	result.origin.y = 0;
@@ -52,17 +61,17 @@ t_sphere new_sphere()
 	result.default_transformation = get_identity_matrix();
 	result.id = globalID;
 	result.material = default_material();
-	globalID ++;
-	return result;
+	globalID++;
+	return (result);
 }
 
-t_xyzvektor set_black(void)
+t_xyzvektor	set_black(void)
 {
-	t_xyzvektor result;
+	t_xyzvektor	result;
 
 	result.x = 0;
 	result.y = 0;
 	result.z = 0;
 	result.w = 1;
-	return result;
+	return (result);
 }
--- initialisation.c
+++ initialisation.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   initialisation.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qhahn <qhahn@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/22 19:29:16 by qhahn             #+#    #+#             */
+/*   Updated: 2025/03/22 19:29:21 by qhahn            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "miniRT.h"
 
-void init_canvas(t_c *canvas)
+void	init_canvas(t_c *canvas)
 {
 	canvas->worldheight = 8;
 	canvas->height = 100;
@@ -13,23 +25,21 @@ void init_canvas(t_c *canvas)
 	canvas->lightsource = default_light();
 }
 
-t_ray init_ray(void)
+t_ray	init_ray(void)
 {
-	t_ray ray;
-	t_xyzvektor origin;
-	t_xyzvektor direction;
+	t_ray		ray;
+	t_xyzvektor	origin;
+	t_xyzvektor	direction;
 
 	origin.x = 0;
 	origin.y = 0;
 	origin.z = -5;
 	origin.w = 1;
-
 	direction.x = 0;
 	direction.y = 0;
 	direction.z = 1;
 	direction.w = 0;
-
 	ray.origin = origin;
 	ray.direction = direction;
-	return ray;
+	return (ray);
 }
