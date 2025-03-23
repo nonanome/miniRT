6ccaab8b99637812093fb17792c64e734e4b0fc1
qhahn <qhahn@student.42.fr>
2025-03-23 11:54:02 +0100

    reduced function linecount in plane and checkerboard parsing

--- initialisation.c
+++ initialisation.c
@@ -6,7 +6,7 @@
 /*   By: qhahn <qhahn@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/22 19:29:16 by qhahn             #+#    #+#             */
-/*   Updated: 2025/03/22 19:29:21 by qhahn            ###   ########.fr       */
+/*   Updated: 2025/03/22 20:15:32 by qhahn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 void	init_canvas(t_c *canvas)
 {
 	canvas->worldheight = 8;
-	canvas->height = 100;
-	canvas->width = 100;
+	canvas->height = 300;
+	canvas->width = 300;
 	canvas->mlx_ptr = mlx_init(canvas->width, canvas->height, "miniRT", false);
 	canvas->pixel_size = canvas->height / canvas->worldheight;
 	canvas->half_size = canvas->worldheight / 2;
