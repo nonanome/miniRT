82fa2526cd695c05e195b38bee2130202dfd533b
qhahn <qhahn@student.42.fr>
2025-04-04 15:47:48 +0200

    norm for is_shadowed function

---
+++ world_building/shade_helpers.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   shade_helpers.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qhahn <qhahn@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/04 15:43:26 by qhahn             #+#    #+#             */
+/*   Updated: 2025/04/04 15:43:36 by qhahn            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "world.h"
+
+void	empty_intersections(t_c *canvas)
+{
+	int	i;
+
+	i = 0;
+	if (canvas->all_intersections.intersections)
+	{
+		while (i < canvas->all_intersections.nr_intersection_entries)
+			FREE(canvas->all_intersections.intersections[i++].times);
+		FREE(canvas->all_intersections.intersections);
+		canvas->all_intersections.nr_intersection_entries = 0;
+		canvas->all_intersections.nr_intersections = 0;
+	}
+	canvas->all_intersections.intersections = NULL;
+}
+
+static t_xyzvektor	calculate_hit_point(t_intersec intersection, double time)
+{
+	t_xyzvektor	result;
+
+	result = addition(intersection.ray.origin,
+			scalar_multiplication(intersection.ray.direction, time));
+	result.w = time;
+	return (result);
+}
+
+t_xyzvektor	hit(t_all_intersec all_intersections)
+{
+	t_xyzvektor	hit_intersection;
+	double		t0;
+	double		t1;
+	int			i;
+
+	i = 0;
+	hit_intersection = set_vector(0, 0, 0, 0);
+	while (i <= all_intersections.nr_intersections / 2 - 1)
+	{
+		t0 = all_intersections.intersections[i].times[0];
+		t1 = all_intersections.intersections[i].times[1];
+		if (t0 > 0 && t0 < t1)
+		{
+			hit_intersection = calculate_hit_point(all_intersections.intersections[i],
+					t0);
+			if (t0 < 0 || t0 > t1)
+			{
+				hit_intersection = calculate_hit_point(all_intersections.intersections[i],
+						t1);
+			}
+			return (hit_intersection);
+		}
+		i++;
+	}
+	return (hit_intersection);
+}
+
+void	sphere_ray_transform(t_shape shape, t_ray ray)
+{
+	double	**inverse_transform;
+
+	if (shape.type != 0)
+	{
+		inverse_transform = invert_matrix(shape.default_transformation, 4);
+		transform_ray(&ray, inverse_transform);
+	}
+}
--- world_building/throw_shade.c
+++ world_building/throw_shade.c
@@ -6,65 +6,24 @@
 /*   By: qhahn <qhahn@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 17:14:50 by qhahn             #+#    #+#             */
-/*   Updated: 2025/04/03 17:36:23 by qhahn            ###   ########.fr       */
+/*   Updated: 2025/04/04 15:43:19 by qhahn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "world.h"
 
-void	empty_intersections(t_c *canvas)
-{
-	int	i;
-
-	i = 0;
-	if (canvas->all_intersections.intersections)
-	{
-		while (i < canvas->all_intersections.nr_intersection_entries)
-			FREE(canvas->all_intersections.intersections[i++].times);
-		FREE(canvas->all_intersections.intersections);
-		canvas->all_intersections.nr_intersection_entries = 0;
-		canvas->all_intersections.nr_intersections = 0;
-	}
-	canvas->all_intersections.intersections = NULL;
-}
-
-static t_xyzvektor	calculate_hit_point(t_intersec intersection, double time)
-{
-	t_xyzvektor	result;
-
-	result = addition(intersection.ray.origin,
-			scalar_multiplication(intersection.ray.direction, time));
-	result.w = time;
-	return (result);
-}
-
-t_xyzvektor	hit(t_all_intersec all_intersections)
+void	check_shade_hit(t_world *world, bool *shadows, double distance, int i)
 {
 	t_xyzvektor	hit_intersection;
-	double		t0;
-	double		t1;
-	int			i;
 
-	i = 0;
-	hit_intersection = set_vector(0, 0, 0, 0);
-	while (i <= all_intersections.nr_intersections / 2 - 1)
-	{
-		t0 = all_intersections.intersections[i].times[0];
-		t1 = all_intersections.intersections[i].times[1];
-		if (t0 > 0 && t0 < t1)
-		{
-			hit_intersection = calculate_hit_point(all_intersections.intersections[i],
-					t0);
-			if (t0 < 0 || t0 > t1)
-			{
-				hit_intersection = calculate_hit_point(all_intersections.intersections[i],
-						t1);
-			}
-			return (hit_intersection);
-		}
-		i++;
-	}
-	return (hit_intersection);
+	if (world->canvas->all_intersections.intersections != NULL)
+		hit_intersection = hit(world->canvas->all_intersections);
+	else
+		hit_intersection.w = INFINITY;
+	if (hit_intersection.w > EPSILON && hit_intersection.w < distance)
+		shadows[i] = true;
+	else
+		shadows[i] = false;
 }
 
 bool	*is_shadowed(t_world *world, t_xyzvektor point, t_shape shape)
@@ -72,11 +31,8 @@ bool	*is_shadowed(t_world *world, t_xyzvektor point, t_shape shape)
 	t_ray		ray;
 	t_xyzvektor	v;
 	double		distance;
-	t_xyzvektor	hit_intersection;
 	int			i;
 	bool		*shadows;
-	double		rotation[3][3];
-	double		**inverse_transform;
 
 	i = -1;
 	shadows = MALLOC(world->canvas->num_lights * sizeof(bool));
@@ -87,21 +43,10 @@ bool	*is_shadowed(t_world *world, t_xyzvektor point, t_shape shape)
 		v = substraction(world->canvas->lightsource[i].position, point);
 		distance = magnitude(v);
 		ray.direction = normalize(v);
-		if (shape.type != 0)
-		{
-			inverse_transform = invert_matrix(shape.default_transformation, 4);
-			transform_ray(&ray, inverse_transform);
-		}
+		sphere_ray_transform(shape, ray);
 		empty_intersections(world->canvas);
 		intersect_world(world, ray);
-		if (world->canvas->all_intersections.intersections != NULL)
-			hit_intersection = hit(world->canvas->all_intersections);
-		else
-			hit_intersection.w = INFINITY;
-		if (hit_intersection.w > EPSILON && hit_intersection.w < distance)
-			shadows[i] = true;
-		else
-			shadows[i] = false;
+		check_shade_hit(world, shadows, distance, i);
 	}
 	empty_intersections(world->canvas);
 	return (shadows);
