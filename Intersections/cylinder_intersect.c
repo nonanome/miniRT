ba5674aedb6ad3aba891584941e78f4412e49ad5
qhahn <qhahn@student.42.fr>
2025-04-04 14:45:36 +0200

    cone and cylinder intersections are now norm conform

--- Intersections/cylinder_intersect.c
+++ Intersections/cylinder_intersect.c
@@ -6,7 +6,7 @@
 /*   By: qhahn <qhahn@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 17:12:29 by qhahn             #+#    #+#             */
-/*   Updated: 2025/04/04 14:19:39 by qhahn            ###   ########.fr       */
+/*   Updated: 2025/04/04 14:35:16 by qhahn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,10 +42,15 @@ void	create_rotation_matrix(t_xyzvektor normal, double **rotation)
 
 void	transform_ray(t_ray *ray, double **rotation)
 {
-	double	origin[3] = {ray->origin.x, ray->origin.y, ray->origin.z};
-	double	direction[3] = {ray->direction.x, ray->direction.y,
-			ray->direction.z};
+	double	origin[3];
+	double	direction[3];
 
+	origin[0] = ray->origin.x;
+	origin[1] = ray->origin.y;
+	origin[2] = ray->origin.z;
+	direction[0] = ray->direction.x;
+	direction[1] = ray->direction.y;
+	direction[2] = ray->direction.z;
 	ray->origin.x = rotation[0][0] * origin[0] + rotation[0][1] * origin[1]
 		+ rotation[0][2] * origin[2];
 	ray->origin.y = rotation[1][0] * origin[0] + rotation[1][1] * origin[1]
@@ -79,6 +84,31 @@ void	cut_cylinder(t_intersec *result, t_ray ray, t_shape shape)
 	return ;
 }
 
+double	get_discriminant(double *discriminant_values, t_ray ray,
+		t_shape cylinder, t_intersec *result)
+{
+	double	discriminant;
+
+	discriminant_values[0] = ray.direction.x * ray.direction.x + ray.direction.z
+		* ray.direction.z;
+	if (discriminant_values[0] > -EPSILON && discriminant_values[0] < EPSILON)
+		return (FREE(discriminant_values), -1);
+	discriminant_values[1] = 2 * ray.origin.x * ray.direction.x + 2
+		* ray.origin.z * ray.direction.z;
+	discriminant_values[2] = ray.origin.x * ray.origin.x + ray.origin.z
+		* ray.origin.z - 1;
+	discriminant = discriminant_values[1] * discriminant_values[1] - 4
+		* discriminant_values[0] * discriminant_values[2];
+	if (discriminant < 0)
+		return (FREE(discriminant_values), -1);
+	result->times[0] = (-discriminant_values[1] - sqrt(discriminant)) / (2
+			* discriminant_values[0]);
+	result->times[1] = (-discriminant_values[1] + sqrt(discriminant)) / (2
+			* discriminant_values[0]);
+	result->object_id = cylinder.id;
+	return (discriminant);
+}
+
 t_intersec	*cylinder_intersect(t_intersec *result, t_ray ray, t_shape cylinder)
 {
 	double	*discriminant_values;
@@ -93,23 +123,9 @@ t_intersec	*cylinder_intersect(t_intersec *result, t_ray ray, t_shape cylinder)
 	transform_ray(&ray, rotation);
 	discriminant_values = MALLOC(3 * sizeof(double));
 	result->times = MALLOC(2 * sizeof(double));
-	discriminant_values[0] = ray.direction.x * ray.direction.x + ray.direction.z
-		* ray.direction.z;
-	if (discriminant_values[0] > -EPSILON && discriminant_values[0] < EPSILON)
-		return (FREE(discriminant_values), NULL);
-	discriminant_values[1] = 2 * ray.origin.x * ray.direction.x + 2
-		* ray.origin.z * ray.direction.z;
-	discriminant_values[2] = ray.origin.x * ray.origin.x + ray.origin.z
-		* ray.origin.z - 1;
-	discriminant = discriminant_values[1] * discriminant_values[1] - 4
-		* discriminant_values[0] * discriminant_values[2];
-	if (discriminant < 0)
-		return (FREE(discriminant_values), NULL);
-	result->times[0] = (-discriminant_values[1] - sqrt(discriminant)) / (2
-			* discriminant_values[0]);
-	result->times[1] = (-discriminant_values[1] + sqrt(discriminant)) / (2
-			* discriminant_values[0]);
-	result->object_id = cylinder.id;
+	discriminant = get_discriminant(discriminant_values, ray, cylinder, result);
+	if (discriminant == -1)
+		return (NULL);
 	cut_cylinder(result, ray, cylinder);
 	if (result->times[0] == -1)
 		cap_top(result, ray, cylinder);
