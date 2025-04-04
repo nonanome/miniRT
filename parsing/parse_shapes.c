a252ade4ada9994cba1b15f1c903a9f0e28c4a99
qhahn <qhahn@student.42.fr>
2025-04-04 14:05:28 +0200

    norm for light parsing and shape parsing

--- parsing/parse_shapes.c
+++ parsing/parse_shapes.c
@@ -6,79 +6,11 @@
 /*   By: qhahn <qhahn@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 17:00:05 by qhahn             #+#    #+#             */
-/*   Updated: 2025/04/03 21:11:27 by qhahn            ###   ########.fr       */
+/*   Updated: 2025/04/04 13:59:15 by qhahn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
-
-int	parse_common_shape(char **split, double pos[3], double rgb[3])
-{
-	char	**pos_split;
-	char	**rgb_split;
-
-	pos_split = ft_split(split[1], ',');
-	if (!pos_split)
-		return (1);
-	rgb_split = ft_split(split[3], ',');
-	if (!rgb_split)
-		return (ft_free_split(pos_split), 1);
-	if (split[0][0] == 'c')
-	{
-		ft_free_split(rgb_split);
-		rgb_split = ft_split(split[5], ',');
-	}
-	if (!parse_xyz(pos_split, pos, 0))
-		return (ft_free_split(pos_split), ft_free_split(rgb_split), 1);
-	if (!parse_xyz(rgb_split, rgb, 0))
-		return (ft_free_split(pos_split), ft_free_split(rgb_split), 1);
-	rgb[0] /= 255;
-	rgb[1] /= 255;
-	rgb[2] /= 255;
-	printf("%f %f %f\n", rgb[0], rgb[1], rgb[2]);
-	if (rgb[0] < 0 || rgb[0] > 1 || rgb[1] < 0 || rgb[1] > 1 || rgb[2] < 0
-		|| rgb[2] > 1)
-		return (ft_free_split(pos_split), ft_free_split(rgb_split), 1);
-	return (ft_free_split(pos_split), ft_free_split(rgb_split), 0);
-}
-
-char	***checker_parsing(char **split, int type)
-{
-	int		i;
-	int		j;
-	char	***split_split;
-
-	i = 0;
-	j = 0;
-	split_split = ft_calloc(4, sizeof(char **));
-	if (!split_split)
-		return (NULL);
-	while (split[i++])
-		;
-	if ((i != 6 && (type == 0 || type == 1)) || (i != 8 && type == 2))
-		return (NULL);
-	if (type == 0 || type == 1)
-		i = 4;
-	else
-		i = 6;
-	while (split[i])
-		split_split[j++] = ft_split(split[i++], ',');
-	return (split_split);
-}
-
-void	add_checker(t_world *world, char **split, int type)
-{
-	char	***split_split;
-
-	split_split = checker_parsing(split, type);
-	if (!split_split)
-		return ;
-	world->shapes[world->nr_shapes
-		- 1]->material.color2 = set_vector(budget_ft_atof(split_split[0][0])
-			/ 255, budget_ft_atof(split_split[0][1]) / 255,
-			budget_ft_atof(split_split[0][2]) / 255, 1);
-	world->shapes[world->nr_shapes - 1]->material.checker_enable = true;
-}
 
 int	parse_sphere(t_world *world, char *line)
 {
@@ -107,21 +39,6 @@ int	parse_sphere(t_world *world, char *line)
 	world->nr_shapes++;
 	add_checker(world, split, 0);
 	return (ft_free_split(split), 0);
-}
-
-static int	parse_normal_vector(char **split, double normal[3])
-{
-	char	**normal_split;
-	int		result;
-
-	normal_split = ft_split(split[2], ',');
-	if (!normal_split)
-		return (1);
-	result = parse_xyz(normal_split, normal, 1) && normal[0] >= -1
-		&& normal[0] <= 1 && normal[1] >= -1 && normal[1] <= 1 && normal[2] >=
-		-1 && normal[2] <= 1;
-	ft_free_split(normal_split);
-	return (!result);
 }
 
 int	parse_plane(t_world *world, char *line)
--- parsing/parsing.h
+++ parsing/parsing.h
@@ -6,7 +6,7 @@
 /*   By: qhahn <qhahn@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 18:03:30 by qhahn             #+#    #+#             */
-/*   Updated: 2025/03/24 17:32:48 by qhahn            ###   ########.fr       */
+/*   Updated: 2025/04/04 13:59:38 by qhahn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,11 @@ int		parse_cone(t_world *world, char *line);
 void	ft_free_split(char **split);
 int		parse_xyz(char **split, double *xyz, int mode);
 int		parse_common_shape(char **split, double pos[3], double rgb[3]);
-
+double	xyz_rgb_brightness(char **split, char **rgb_split, double rgb[4],
+			double xyz[3]);
+int		parse_fov(char *str, double *fov);
+int		parse_xyz_cam_light(char *str, double xyz[3], int is_normal);
+void	add_checker(t_world *world, char **split, int type);
 void	check_camera_line(char *line);
 void	check_spere_or_light_line(char *line);
 int		check_plane_line(char *line);
@@ -39,3 +43,5 @@ void	check_ambient_light_line(char *line);
 int		parse_rgb(char *str, double rgb[3]);
 int		parse_ratio(char *str, double *ratio);
 int		count_split(char **split_string);
+int		parse_common_shape(char **split, double pos[3], double rgb[3]);
+int		parse_normal_vector(char **split, double normal[3]);
