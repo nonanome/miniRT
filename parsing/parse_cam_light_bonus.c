04f3036631e5b68eb93e1a861a6b45d831dbd117
qhahn <qhahn@student.42.fr>
2025-04-19 14:30:38 +0200

    added copies of parsing and light parsing for bonus norm

---
+++ parsing/parse_cam_light_bonus.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_cam_light.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qhahn <qhahn@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/14 16:58:46 by qhahn             #+#    #+#             */
+/*   Updated: 2025/04/19 13:17:20 by qhahn            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parsing.h"
+
+int	parse_ambient_light(t_world *world, char *line)
+{
+	char	**split;
+	int		error;
+	double	ratio;
+	double	rgb[3];
+
+	check_ambient_light_line(line);
+	split = ft_split(line, ' ');
+	if (!split)
+		return (1);
+	error = parse_ratio(split[1], &ratio) || parse_rgb(split[2], rgb);
+	if (!error)
+	{
+		*(world->ambient) = set_vector(rgb[0], rgb[1], rgb[2], 0);
+		world->ambient_intensity = ratio;
+	}
+	ft_free_split(split);
+	return (error);
+}
+
+int	parse_camera(t_world *world, char *line)
+{
+	char		**split;
+	double		xyz[3];
+	double		normal[3];
+	t_xyzvektor	target;
+
+	check_camera_line(line);
+	split = ft_split(line, ' ');
+	if (!split)
+		return (1);
+	if (budget_ft_atof(split[3]) < 0 || budget_ft_atof(split[3]) > 180)
+		bail("fov between 70 and 180", 1);
+	if (parse_xyz_cam_light(split[1], xyz, 0))
+		return (ft_free_split(split), 1);
+	if (parse_xyz_cam_light(split[2], normal, 1))
+		return (ft_free_split(split), 1);
+	world->camera = camera(world->canvas->width, world->canvas->height,
+			budget_ft_atof(split[3]));
+	if (!world->camera)
+		return (1);
+	target = addition(set_vector(xyz[0], xyz[1], xyz[2], 1),
+			normalize(set_vector(normal[0], normal[1], normal[2], 0)));
+	world->camera->transform = view_transform(set_vector(xyz[0], xyz[1], xyz[2],
+				1), target, set_vector(0, 1, 0, 0));
+	return (ft_free_split(split), 0);
+}
+
+void	realloc_light(t_c *canvas, t_light new_light)
+{
+	t_light	*lights;
+	int		i;
+
+	i = 0;
+	lights = MALLOC((canvas->num_lights + 1) * sizeof(t_light));
+	while (i < canvas->num_lights)
+	{
+		lights[i] = canvas->lightsource[i];
+		i++;
+	}
+	lights[i] = new_light;
+	FREE(canvas->lightsource);
+	canvas->lightsource = lights;
+	canvas->num_lights++;
+}
+
+int	parse_light(t_world *world, char *line)
+{
+	char	**split;
+	double	xyz[3];
+	double	rgb[4];
+	t_light	new_light;
+	char	**rgb_split;
+
+	check_light_line(line);
+	split = ft_split(line, ' ');
+	if (!split)
+		return (1);
+	if (budget_ft_atof(split[2]) < 0)
+		return (ft_free_split(split), 1);
+	rgb_split = ft_split(split[3], ',');
+	if (!rgb_split)
+		return (ft_free_split(split), 1);
+	if (xyz_rgb_brightness(split, rgb_split, rgb, xyz))
+		return (write(2, "problem parsing light\n", 22), 1);
+	new_light.color = set_vector(rgb[0], rgb[1], rgb[2], 0);
+	new_light.brightness = budget_ft_atof(split[1]);
+	new_light.position = set_vector(xyz[0], xyz[1], xyz[2], 0);
+	realloc_light(world->canvas, new_light);
+	return (ft_free_split(split), ft_free_split(rgb_split), 0);
+}
---
+++ parsing/parsing_bonus.c
@@ -0,0 +1,121 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qhahn <qhahn@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/10 16:54:58 by qhahn             #+#    #+#             */
+/*   Updated: 2025/04/18 13:12:19 by qhahn            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parsing.h"
+
+void	look_for_double_uppercase(char **input, int A, int C, int L)
+{
+	while (*input)
+	{
+		if (*input[0] == 'A')
+			A++;
+		if (*input[0] == 'C')
+			C++;
+		if (*input[0] == 'L')
+			L++;
+		else if (*input[0] >= '0' && *input[0] <= '9')
+		{
+			bail("first characters needs to be char", 1);
+		}
+		input++;
+	}
+	if (A > 1 || C > 1)
+	{
+		write(2, "input contains double capital letter object",
+			ft_strlen("input contains double capital letter object"));
+		exit(0);
+	}
+}
+
+char	**process_file(char *file_name)
+{
+	char	**input;
+	int		fd;
+	char	*line;
+	int		i;
+
+	i = 0;
+	fd = open(file_name, O_RDONLY);
+	if (fd == -1)
+		bail("file not found", 1);
+	input = MALLOC(1000 * sizeof(char *));
+	if (!input)
+		return (NULL);
+	while (budget_get_next_line(fd, &line) > 0)
+	{
+		input[i] = line;
+		i++;
+	}
+	input[i] = line;
+	input[++i] = NULL;
+	look_for_double_uppercase(input, 0, 0, 0);
+	close(fd);
+	return (input);
+}
+
+static void	free_input(char **input)
+{
+	int	i;
+
+	i = 0;
+	if (!input)
+		return ;
+	while (input[i])
+		FREE(input[i++]);
+	FREE(input);
+}
+
+static int	parse_line(t_world *world, char *line)
+{
+	if (line[0] == 'A')
+		return (parse_ambient_light(world, line));
+	if (line[0] == 'C')
+		return (parse_camera(world, line));
+	if (line[0] == 'L')
+		return (parse_light(world, line));
+	if (!ft_strncmp(line, "sp", 2))
+		return (parse_sphere(world, line));
+	if (!ft_strncmp(line, "pl", 2))
+		return (parse_plane(world, line));
+	if (!ft_strncmp(line, "cy", 2))
+		return (parse_cylinder(world, line));
+	return (0);
+}
+
+int	parse_input(char *file_name, t_world *world)
+{
+	char	**input;
+	char	*cleaned;
+	int		i;
+
+	i = 0;
+	world->all_sorted = ft_calloc(sizeof(double *), 100);
+	if (!world->all_sorted)
+		return (1);
+	world->all_sorted[0] = 0;
+	input = process_file(file_name);
+	if (!input)
+		return (1);
+	while (input[i])
+	{
+		cleaned = clean_input_line(input[i]);
+		if ((parse_line(world, cleaned)))
+			return (1);
+		else if (ft_strncmp(cleaned, "co", 2) == 0)
+		{
+			if (parse_cone(world, cleaned))
+				return (1);
+		}
+		i++;
+	}
+	return (free_input(input), 0);
+}
